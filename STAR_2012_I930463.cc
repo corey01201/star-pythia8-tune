34fd13c2b753455aadea01fe480838d5502ec844
Christian Gutschow <chris.g@cern.ch>
2025-07-08 16:19:23 -0400

    migrate to R4/Y2

--- STAR_2012_I930463.cc
+++ STAR_2012_I930463.cc
@@ -1,11 +1,6 @@
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
-#include "Rivet/Projections/ChargedFinalState.hh"
-//! c++ headers
-#include <iostream>     
-#include <algorithm>    
-#include <vector>
 
 namespace Rivet {
 
@@ -19,90 +14,72 @@ namespace Rivet {
       void init() {
 
         // Initialise and register projections
-        const ChargedFinalState cfs(Cuts::abseta < 1.5 && Cuts::abscharge > 0);
-        declare(cfs, "CFS");
         const FinalState fs(Cuts::abseta < 1.5);
         declare(fs, "FS");
 
         // Book histograms
-        book(_h_pT_piplus ,1, 1, 1);
-        book(_h_pT_piminus,1, 1, 2);
-        book(_h_pT_kplus  ,1, 1, 3);
-        book(_h_pT_kminus ,1, 1, 4);
-        book(_h_pT_p      ,1, 1, 5);
-        book(_h_pT_pbar   ,1, 1, 6);
-        // book(_h_pT_Kshort ,2, 1, 1);
-        // book(_h_pT_pho    ,3, 1, 1);
-        book(_h_ratio_pimpip ,7,1,1);
-        book(_h_ratio_pbarp ,7,1,2);
-        book(_h_ratio_KmKp ,7,1,3);
-        book(_h_ratio_ppi ,7,1,4);
-        book(_h_ratio_pbarpibar ,7,1,5);
-        book(_h_ratio_Kpi ,7,1,6);
-        book(_h_K,"TMP/K+K-" ,refData(7,1,6));
-        book(_h_pi,"TMP/pi+pi-" ,refData(7,1,6));
-
-        book(_sumWeightSelected, "TMP/sumWselected");
+        book(_h["pT_piplus"],    1, 1, 1);
+        book(_h["pT_piminus"],   1, 1, 2);
+        book(_h["pT_kplus"],     1, 1, 3);
+        book(_h["pT_kminus"],    1, 1, 4);
+        book(_h["pT_p"],         1, 1, 5);
+        book(_h["pT_pbar"],      1, 1, 6);
+        book(_e["ratio_pimpip"],    7,1,1);
+        book(_e["ratio_pbarp"],     7,1,2);
+        book(_e["ratio_KmKp"],      7,1,3);
+        book(_e["ratio_ppi"],       7,1,4);
+        book(_e["ratio_pbarpibar"], 7,1,5);
+        book(_e["ratio_Kpi"],       7,1,6);
+        book(_h["K"],  "TMP/K+K-",   refData(7,1,6));
+        book(_h["pi"], "TMP/pi+pi-", refData(7,1,6));
 
       }
 
       /// Perform the per-event analysis
       void analyze(const Event& event) {
 
-        const FinalState& finalstate = apply<FinalState>(event, "FS");
+        for (const Particle& p: apply<FinalState>(event, "FS").particles()) {
 
-        for(Particle p: finalstate.particles()){
-
-          if (p.absrap() < 0.5 && p.pT()/GeV>3) {
+          if (p.absrap() < 0.5 && p.pT()>3*GeV) {
             const PdgId pid = p.pid();
             const double pT = p.pT() / GeV;
             switch (abs(pid)) {
               case PID::PIPLUS:
-                _h_pi->fill(pT, (1./(2.0 * M_PI))/pT);
-                pid > 0 ? _h_pT_piplus->fill(pT, (1./(2.0 * M_PI))/pT) : _h_pT_piminus->fill(pT, (1./(2.0 * M_PI))/pT);
+                _h["pi"]->fill(pT, 1.0/TWOPI/pT);
+                _h[pid > 0? "pT_piplus" : "pT_piminus"]->fill(pT, 1.0/TWOPI/pT);
                 break;
               case PID::PROTON:
-                pid > 0 ? _h_pT_p->fill(pT, (1./(2.0 * M_PI))/pT) : _h_pT_pbar->fill(pT, (1./(2.0 * M_PI))/pT);
+                _h[pid > 0? "pT_p" : "pT_pbar"]->fill(pT, 1.0/TWOPI/pT);
                 break;
               case PID::KPLUS:
-                _h_K->fill(pT, 1.0/pT);
-                pid > 0 ? _h_pT_kplus->fill(pT, (1./(2.0 * M_PI))/pT) : _h_pT_kminus->fill(pT, (1./(2.0 * M_PI))/pT);
+                _h["K"]->fill(pT, 1.0/pT);
+                _h[pid > 0? "pT_kplus" : "pT_kminus"]->fill(pT, 1.0/TWOPI/pT);
                 break;
             }
           }
 
-        } //end of loop FS particle 
+        } //end of loop FS particle
 
-        _sumWeightSelected->fill();
       } // end event ana
 
       /// Normalise histograms etc., after the run
       void finalize() {
-	
-        double ppCrossSection = 30*1e3*microbarn; // STAR pp MB 
 
-        const YODA::Scatter1D factor = 1. / *_sumWeightSelected;
+        scale(_h, 1.0/sumOfWeights());
 
-        scale(_h_pT_piplus,       factor);
-        scale(_h_pT_piminus,       factor);
-        scale(_h_pT_kminus,       factor);
-        scale(_h_pT_kplus,       factor);
-        scale(_h_pT_p,       factor);
-        scale(_h_pT_pbar,       factor);
-
-        divide(_h_pT_piminus, _h_pT_piplus, _h_ratio_pimpip);
-        divide(_h_pT_pbar, _h_pT_p, _h_ratio_pbarp);
-        divide(_h_pT_kminus, _h_pT_kplus, _h_ratio_KmKp);
-        divide(_h_pT_p, _h_pT_piplus, _h_ratio_ppi);
-        divide(_h_pT_pbar, _h_pT_piminus, _h_ratio_pbarpibar);
-        divide(_h_K, _h_pi, _h_ratio_Kpi);
+        divide(_h["pT_piminus"], _h["pT_piplus"],  _e["pimpip"]);
+        divide(_h["pT_pbar"],    _h["pT_p"],       _e["pbarp"]);
+        divide(_h["pT_kminus"],  _h["pT_kplus"],   _e["KmKp"]);
+        divide(_h["pT_p"],       _h["pT_piplus"],  _e["ppi"]);
+        divide(_h["pT_pbar"],    _h["pT_piminus"], _e["pbarpibar"]);
+        divide(_h["K"],          _h["pi"],         _e["Kpi"]);
 
       }
 
     private:
-      CounterPtr _sumWeightSelected;
-      Histo1DPtr _h_pT_piplus, _h_pT_piminus, _h_pT_kminus, _h_pT_kplus, _h_pT_p, _h_pT_pbar, _h_K,_h_pi;
-      Scatter2DPtr _h_ratio_pimpip, _h_ratio_pbarp, _h_ratio_ppi, _h_ratio_KmKp, _h_ratio_pbarpibar, _h_ratio_Kpi;
+
+      map<string,Histo1DPtr> _h;
+      map<string,Estimate1DPtr> _e;
   };
 
 
